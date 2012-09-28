e37271929b8b9f0ec3ca344c8d9dd28fe15d1936
Douglas Jarquin <douglasjarquin@me.com>
2012-09-28 11:36:56 -0400

    Return ElasticIPs if the public hostname is empty

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -12,7 +12,7 @@ module Capistrano
         @ec2 ||= RightAws::Ec2.new(fetch(:aws_access_key_id), fetch(:aws_secret_access_key), fetch(:aws_params, {}))
 
         @ec2.describe_instances(:filters => { "tag-key" => "deploy", "tag-value" => "#{which}" }).each do |instance|
-          server(instance[:dns_name], *args) unless instance[:aws_state] != "running"
+          server(instance[:dns_name].empty? ? instance[:ip_address] : instance[:dns_name], *args) unless instance[:aws_state] != "running"
         end
       end
 
