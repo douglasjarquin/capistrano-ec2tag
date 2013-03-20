f2b00044e04095bf772f020910cd3f9f95617f78
Alfred Moreno <alfred.moreno@zumba.com>
2013-03-20 13:04:15 -0400

    Refactor capistrano-ec2tag to use aws-sdk

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -1,4 +1,4 @@
-require 'right_aws'
+require 'aws-sdk'
 
 unless Capistrano::Configuration.respond_to?(:instance)
   abort 'capistrano/ec2tag requires Capistrano >= 2'
@@ -9,11 +9,12 @@ module Capistrano
     module Tags
 
       def tag(which, *args)
-        @ec2 ||= RightAws::Ec2.new(fetch(:aws_access_key_id), fetch(:aws_secret_access_key), fetch(:aws_params, {}))
+        @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params))
 
-        @ec2.describe_instances(:filters => { 'tag-key' => 'deploy', 'tag-value' => "#{which}" }).each do |instance|
-          server(instance[:dns_name].empty? ? instance[:ip_address] : instance[:dns_name], *args) unless instance[:aws_state] != 'running'
-        end
+        @ec2.instances.filter('tag-key','deploy').filter('tag-value', which).each { |instance|
+          server instance.dns_name || instance.ip_address, *args if instance.status == :running
+        }
+
       end
 
     end
