fbc58894e7c3aae00a8648daaf6ab3d80aac04a6
Douglas Jarquin <douglas.jarquin@zumba.com>
2013-08-09 13:26:39 -0400

    Check for the private ip if public ip is empty

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -12,7 +12,7 @@ module Capistrano
         @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params, {}))
 
         @ec2.instances.filter('tag-key', 'deploy').filter('tag-value', which).each do |instance|
-          server instance.dns_name || instance.ip_address, *args if instance.status == :running
+          server instance.ip_address || instance.private_ip_address, *args if instance.status == :running
         end
       end
 
