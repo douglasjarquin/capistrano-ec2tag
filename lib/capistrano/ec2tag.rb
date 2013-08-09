eec4e7c15e6069093d4a01ac4e339ce40229dd36
Douglas Jarquin <douglas.jarquin@zumba.com>
2013-08-09 14:04:36 -0400

    Load aws credentials from environment variables by default

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -7,6 +7,8 @@ end
 module Capistrano
   class Configuration
     module Tags
+      _cset(:access_key_id, ENV['AWS_ACCESS_KEY_ID'])
+      _cset(:secret_access_key, ENV['AWS_SECRET_ACCESS_KEY'])
 
       def tag(which, *args)
         @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params, {}))
