35217aeae7217866383140e41529eba52bd55325
Osamu MATSUMOTO <osamu.matsumoto@mixi.co.jp>
2015-03-24 17:39:19 +0900

    Cache instance lists avoid fetch each time

--- lib/capistrano/ec2tag.rb
+++ lib/capistrano/ec2tag.rb
@@ -12,10 +12,26 @@ module Capistrano
           def tag(which, *args)
             @ec2 ||= AWS::EC2.new({access_key_id: fetch(:aws_access_key_id), secret_access_key: fetch(:aws_secret_access_key)}.merge! fetch(:aws_params, {}))
 
-            @ec2.instances.filter('tag-key', 'deploy').filter('tag-value', which).each do |instance|
-              server instance.ip_address || instance.private_ip_address, *args if instance.status == :running
+            @target_instances = ec2_instances('deploy') unless @target_instances
+
+            if @target_instances[which]
+              @target_instances[which].each do |ip, status|
+                server ip, *args if status == :running
+              end
             end
           end
+
+          def ec2_instances(tag)
+            AWS.memoize do
+              return @ec2.instances.filter('tag-key', tag).inject({}) do |res,instance|
+                tag_name = instance.tags.to_h[tag]
+                res[tag_name] ||= []
+                res[tag_name] << [ instance.private_ip_address, instance.status]
+                res
+              end
+            end
+          end
+
         end
       end
     end
